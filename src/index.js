580dafaac1431ba07f14059b5691557d10ec3208
Naoya <33260607+naoya-o-git@users.noreply.github.com>
2023-10-13 11:35:46 +0000

    uncomment logical operator part for submit

--- src/index.js
+++ src/index.js
@@ -179,19 +179,19 @@
 /**
  * 論理演算子
  */
-// const flag1 = true;
-// const flag2 = false;
-// if (flag1 || flag2) {
-//   console.log("1か2はtrue");
-// }
-// if (flag1 && flag2) {
-//   console.log("1も2もtrue");
-// }
+const flag1 = true;
+const flag2 = false;
+if (flag1 || flag2) {
+  console.log("1か2はtrue");
+}
+if (flag1 && flag2) {
+  console.log("1も2もtrue");
+}
 
-// const num = 100;
-// const fee = num1 || "金額未設定です";
-// console.log(fee);
+const num = 100;
+const fee = num1 || "金額未設定です";
+console.log(fee);
 
-// const num2 = null;
-// const fee2 = num2 && "何か設定されました";
-// console.log(fee2);
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
